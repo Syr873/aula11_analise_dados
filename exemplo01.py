532fe86f44f6dc13788832a85d6119198bc92e2f
Syr873 <mtroi873@gmail.com>
2026-01-06 19:46:45 -0300

    biblioteca os para limpar a tela

--- exemplo01.py
+++ exemplo01.py
@@ -1,21 +1,23 @@
 # Bibliotecas
 import random
+import os
 
 n = random.randint(1, 10)
 m = random.randint(1, 10)
 
-# print(n, m)
+print(n, m)
 
 # ------------------------------------------------------------
 
 # Gerar numeros decimais
 n_decimal = random.uniform(1, 10)
 numero_decimal = round(n_decimal, 1) # Arredonda
-# print(f'{n_decimal:.2f}') # Formata
-# print(numero_decimal)
+print(f'{n_decimal:.2f}') # Formata
+print(numero_decimal)
 
 # ------------------------------------------------------------
 
 # Gerar numeros aleatorios dentro de uma lista
-lst_numeros = [random.randint(1, 10) for num in range(5)]
+lst_numeros = [random.randint(1, 50) for num in range(5)]
+os.system('cls') # limpa a tela antes de printar a lista
 print(lst_numeros)
