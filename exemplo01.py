235cd01de347e4a32f021fb76abb39418e03925e
Syr873 <mtroi873@gmail.com>
2026-01-06 19:35:04 -0300

    random for e list para gerar numeros aleatorios

--- exemplo01.py
+++ exemplo01.py
@@ -11,5 +11,11 @@ m = random.randint(1, 10)
 # Gerar numeros decimais
 n_decimal = random.uniform(1, 10)
 numero_decimal = round(n_decimal, 1) # Arredonda
-print(f'{n_decimal:.2f}') # Formata
-print(numero_decimal)
+# print(f'{n_decimal:.2f}') # Formata
+# print(numero_decimal)
+
+# ------------------------------------------------------------
+
+# Gerar numeros aleatorios dentro de uma lista
+lst_numeros = [random.randint(1, 10) for num in range(5)]
+print(lst_numeros)
